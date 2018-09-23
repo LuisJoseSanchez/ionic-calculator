fa9bdfbf92faaf9b830987624ad0e18f39b32e41
Luis José Sánchez <luisjose@espiral.io>
2018-09-23 22:52:38 +0200

    Sum working.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  display = '0';
+  memory = 0;
+  state = 'number';
+  operator: string;
+
+  clickNumber(n: number) {
+    switch (this.state) {
+      case 'number':
+        this.display = (+this.display * 10 + n).toString();
+        break;
+      case 'operator':
+        this.display = n.toString();
+        this.state = 'number';
+        break;
+      case 'result':
+        this.display = n.toString();
+        this.state = 'number';
+    }
+  }
+
+  clickOperator(o: string) {
+    this.state = 'operator';
+    this.operator = o;
+    this.memory = +this.display;
+    this.display = o;
+  }
+
+  calculate() {
+    // tslint:disable-next-line:no-eval
+    this.display = eval(this.memory + this.operator + this.display).toString();
+    this.state = 'result';
+  }
 }
